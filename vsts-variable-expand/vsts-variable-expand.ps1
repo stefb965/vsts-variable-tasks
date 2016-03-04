14dbb72d54d31ce6efe08f713e67a85626d8f4c1
Jesse Houwing <jesse.houwing@gmail.com>
2016-03-04 16:18:35 +0100

    Initial checkin of the task skeletons.

---
+++ vsts-variable-expand/vsts-variable-expand.ps1
@@ -0,0 +1,16 @@
+﻿[cmdletbinding()]
+param(
+    
+)
+
+Write-Verbose "Entering script $($MyInvocation.MyCommand.Name)"
+Write-Verbose "Parameter Values"
+$PSBoundParameters.Keys | %{ Write-Verbose "$_ = $($PSBoundParameters[$_])" }
+
+Write-Verbose "Importing modules"
+Import-Module -DisableNameChecking "$PSScriptRoot/vsts-extension-shared.psm1"
+
+Write-Debug "Setting output variable '$($packageOptions.OutputVariable)' to '$($output.packaged)'"
+Write-Host "##vso[task.setvariable variable=$($packageOptions.OutputVariable);]$($output.packaged)"
+
+Write-Host "##vso[task.complete result=Succeeded;]DONE"
