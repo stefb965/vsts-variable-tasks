5ab14ed7342e97a82c0762099f26480c815dab91
Jesse Houwing <jesse.houwing@gmail.com>
2016-09-01 14:07:21 +0200

    Log a warning on Agent 2 instead of throwign exceptions (#13)
    * Logs a warning on Agent 2.0 when using the Expand Variable task.
    
    Because the 2.0 agent will auto-expand variables, it should no longer need to run this task. Also switches the Variable Set to the Node implementation.
    
    * Uses Powershell 3 host to log warning on 2.0 agents.
    
    * Removes Node implementation of Expand Variables
    
    * need to put the old handler first it seems
    
    * Update release note
    
    * Make the task Succeed with Issues on 2.0 agents.

---
+++ vsts-variable-expand/vsts-variable-expand.v3.ps1
@@ -0,0 +1,4 @@
+﻿import-module .\ps_modules\vststasksdk\vststasksdk.psd1
+
+Write-VstsTaskWarning -Message "Expand Variable task is no longer required. The 2.0 agent will automatically expand variables."
+Write-VstsSetResult -Result "SucceededWithIssues"
