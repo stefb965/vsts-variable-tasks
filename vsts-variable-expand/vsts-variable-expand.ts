fadb7aa18294fdf7023aa2175973e4888faa59c0
Jesse Houwing <jesse.houwing@gmail.com>
2016-09-01 13:20:14 +0200

    Removes Node implementation of Expand Variables

--- vsts-variable-expand/vsts-variable-expand.ts
+++
@@ -1,4 +0,0 @@
-﻿///<reference path="./typings/main.d.ts" />
-import tl = require("vsts-task-lib/task");
-
-tl.warning("Expand Variable task is no longer required. The 2.0 agent will automatically expand variables.");
