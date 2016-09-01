09e8c470f5f85697361c95f1a974acf1ed7ef705
Jesse Houwing <jesse.houwing@gmail.com>
2016-09-01 11:53:18 +0200

    Logs a warning on Agent 2.0 when using the Expand Variable task.
    Because the 2.0 agent will auto-expand variables, it should no longer need to run this task. Also switches the Variable Set to the Node implementation.

---
+++ vsts-variable-expand/vsts-variable-expand.ts
@@ -0,0 +1,4 @@
+﻿///<reference path="./typings/main.d.ts" />
+import tl = require("vsts-task-lib/task");
+
+tl.warning("Expand Variable task is no longer required. The 2.0 agent will automatically expand variables.");
